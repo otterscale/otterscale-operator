69081bf2edacfac8edb064f3402c085c5ce8d31d
Paul Tsai <paul_tsai@phison.com>
2026-01-16 02:55:20 +0000

    feat: define Workspace API with user roles and resource management

--- api/v1alpha1/workspace_types.go
+++ api/v1alpha1/workspace_types.go
@@ -17,41 +17,77 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// WorkspaceUser defines the user of a workspace.
+type WorkspaceUser struct {
+	// Subject is the unique identifier of the user (e.g. oidc subject).
+	// +kubebuilder:validation:MinLength=1
+	// +required
+	Subject string `json:"subject"`
+
+	// Name is the display name of the user.
+	// +optional
+	Name *string `json:"name,omitempty"`
+}
 
 // WorkspaceSpec defines the desired state of Workspace
 type WorkspaceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
+	// Admins are the admins of the workspace.
+	// +listType=map
+	// +listMapKey=subject
+	// +kubebuilder:validation:MinItems=1
+	// +required
+	Admins []WorkspaceUser `json:"admins,omitempty"`
 
-	// foo is an example field of Workspace. Edit workspace_types.go to remove/update
+	// Editors are the editors of the workspace.
+	// +listType=map
+	// +listMapKey=subject
 	// +optional
-	Foo *string `json:"foo,omitempty"`
+	Editors []WorkspaceUser `json:"editors,omitempty"`
+
+	// Viewers are the viewers of the workspace.
+	// +listType=map
+	// +listMapKey=subject
+	// +optional
+	Viewers []WorkspaceUser `json:"viewers,omitempty"`
+
+	// ResourceQuota defines the resource quota for the workspace.
+	// +optional
+	ResourceQuota *corev1.ResourceQuotaSpec `json:"resourceQuota,omitempty"`
+
+	// LimitRange defines the limit range for the workspace.
+	// +optional
+	LimitRange *corev1.LimitRangeSpec `json:"limitRange,omitempty"`
+
+	// NetworkIsolationEnabled indicates whether network isolation is enabled.
+	// +optional
+	NetworkIsolationEnabled *bool `json:"networkIsolationEnabled,omitempty"`
 }
 
 // WorkspaceStatus defines the observed state of Workspace.
 type WorkspaceStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Namespace is the namespace associated with the workspace.
+	// +optional
+	Namespace *corev1.ObjectReference `json:"namespace,omitempty"`
 
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+	// ResourceQuota is the resource quota object associated with the workspace.
+	// +optional
+	ResourceQuota *corev1.ObjectReference `json:"resourceQuota,omitempty"`
 
-	// conditions represent the current state of the Workspace resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
+	// LimitRange is the limit range object associated with the workspace.
+	// +optional
+	LimitRange *corev1.ObjectReference `json:"limitRange,omitempty"`
+
+	// RoleBindings are the role bindings associated with the workspace.
+	// +listType=map
+	// +listMapKey=name
+	// +optional
+	RoleBindings []corev1.ObjectReference `json:"roleBindings,omitempty"`
+
+	// Conditions represent the current state of the Workspace resource.
 	// +listType=map
 	// +listMapKey=type
 	// +optional
@@ -60,7 +96,8 @@ type WorkspaceStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="Namespace",type=string,JSONPath=`.status.namespace.name`
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
 // Workspace is the Schema for the workspaces API
 type Workspace struct {
